d6364e5cfa207653af18571e5e7323324df1c00d
yasser mimouni <yasmimouni2@gmail.com>
2017-11-05 20:18:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(arr)
 {
-  for (var i = 0; i < 25 ; i++) {
+  for (var i = arr.length - 1; i < 25 ; i++) {
     arr[i] = "I am ${i} strange loop${i === 0 ? '' : 's'}."
   }
   return arr;
